958fb99a53d7729ef887a84b9c603b4f4c6e0c5f
yixoaqi <qixiaoyi@outlook.com>
2017-02-28 14:14:30 +0800

    singup1

--- app/src/main/java/com/qixiaoyi/cariyou/WelcomeActivity.java
+++ app/src/main/java/com/qixiaoyi/cariyou/WelcomeActivity.java
@@ -1,15 +1,13 @@
 package com.qixiaoyi.cariyou;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashActivity extends Activity {
+public class WelcomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        startActivity(new Intent(SplashActivity.this,WelcomeActivity.class));
+        setContentView(R.layout.activity_welcome);
     }
 }
