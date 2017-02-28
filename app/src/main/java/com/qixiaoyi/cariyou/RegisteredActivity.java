ed9e4554d8d6c8e49f9f97663581c6e9ed5817dd
yixoaqi <qixiaoyi@outlook.com>
2017-03-01 01:16:08 +0800

    T1基本界面01

---
+++ app/src/main/java/com/qixiaoyi/cariyou/RegisteredActivity.java
@@ -0,0 +1,13 @@
+package com.qixiaoyi.cariyou;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisteredActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registered);
+    }
+}
--- app/src/main/java/com/qixiaoyi/cariyou/WelcomeActivity.java
+++ app/src/main/java/com/qixiaoyi/cariyou/WelcomeActivity.java
@@ -1,13 +1,41 @@
 package com.qixiaoyi.cariyou;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends Activity {
 
+    Button bt_singup;
+    Button bt_visitor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        bt_singup = (Button) findViewById(R.id.bt_singup);
+        bt_visitor = (Button) findViewById(R.id.bt_visitor);
+        bt_singup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeActivity.this,RegisteredActivity.class));
+            }
+        });
+        bt_visitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeActivity.this,LoginActivity.class));
+            }
+        });
+    }
+
+    public void singup(View view){
+    }
+
+    public void visitor(View view){
+    }
+    public void startlogin(View view){
+
     }
 }
